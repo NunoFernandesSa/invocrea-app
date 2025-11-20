2261cf34b645ce619bb314a74b9e1cdd7b4234a4
NunoF <n.fernandes.contact@gmail.com>
2025-11-20 09:49:27 +0100

    feat: refactor dashboard page layout, update button utility import, and implement user check/add functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import Container from "@/src/components/common/Container";
+
 export default function DashboardPage() {
   return (
-    <>
+    <Container>
       <h2>Dashboard</h2>
-    </>
+    </Container>
   );
 }
