2ed3cc96cbcd5979dd613603ccd41037bcb87c3c
NunoF <n.fernandes.contact@gmail.com>
2025-11-20 21:56:27 +0100

    feat: add user greeting and new invoice form to dashboard
    - Implemented user greeting in the dashboard based on authentication status.
    - Integrated NewInvoiceForm component for creating new invoices.
    - Refactored utility functions for class name merging into a new location.
    - Added Dialog, Input, Label, and Button components using Shadcn UI.
    - Created a new form for invoice creation with appropriate fields and actions.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import Container from "@/src/components/common/Container";
+import { NewInvoiceForm } from "@/src/forms/NewInvoiceForm";
+import { useUser } from "@clerk/nextjs";
 
 export default function DashboardPage() {
+  const user = useUser();
+
   return (
     <Container>
-      <h2>Dashboard</h2>
+      {/* show user name is signed in */}
+      {user && (
+        <div>
+          <h2 className="text-xl font-bold mb-4">
+            Welcome {user.user?.fullName}!
+          </h2>
+          <p>This is your dashboard where you can manage your invoices.</p>
+        </div>
+      )}
+
+      <div className="">
+        <NewInvoiceForm />
+      </div>
     </Container>
   );
 }
---
+++ src/forms/NewInvoiceForm.tsx
@@ -0,0 +1,57 @@
+// ----- Shadcn -----
+import { Button } from "../components/shadcn/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/shadcn/ui/dialog";
+import { Input } from "../components/shadcn/ui/input";
+import { Label } from "../components/shadcn/ui/label";
+
+// ----- Icons -----
+import { MdAdd } from "react-icons/md";
+
+export function NewInvoiceForm() {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline">
+            New Invoice <MdAdd />
+          </Button>
+        </DialogTrigger>
+
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Create new invoice</DialogTitle>
+            <DialogDescription>
+              Fill the form below to create a new invoice.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            {/* ----- name ----- */}
+            <div className="grid gap-3">
+              <Label htmlFor="invoice-name">Name</Label>
+              <Input
+                id="invoice-name"
+                name="invoice-name"
+                defaultValue="Invoice Name"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
