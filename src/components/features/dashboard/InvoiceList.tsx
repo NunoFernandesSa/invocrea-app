04ec062e5c012c5756ec6e5b864489883576cfe7
NunoF <n.fernandes.contact@gmail.com>
2025-11-22 13:16:37 +0100

    feat: Add InvoiceList component to display invoice cards in a grid layout.

--- src/components/features/dashboard/InvoiceList.tsx
+++ src/components/features/dashboard/InvoiceList.tsx
@@ -2,7 +2,7 @@ import InvoiceCard from "./InvoiceCard";
 
 export default function InvoiceList() {
   return (
-    <div className="flex flex-col justify-center md:justify-start items-center md:items-start md:flex-row flex-wrap gap-4 mt-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
       <InvoiceCard />
     </div>
   );
