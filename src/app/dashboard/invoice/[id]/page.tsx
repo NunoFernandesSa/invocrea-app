10b0f3e31af31afbbbb272becf551794dc41f5f1
NunoF <n.fernandes.contact@gmail.com>
2025-11-26 02:55:20 +0100

    feat: add invoice details page with loading and error handling for displaying individual invoices

--- src/app/dashboard/invoice/[id]/page.tsx
+++ src/app/dashboard/invoice/[id]/page.tsx
@@ -2,11 +2,12 @@
 
 import Container from "@/src/components/common/Container";
 import GoBackButton from "@/src/components/features/invoices/GoBackButton";
+import { Badge } from "@/src/components/shadcn/ui/badge";
 import { Spinner } from "@/src/components/shadcn/ui/spinner";
 import { InvoiceDetailsProps } from "@/src/types/invoice-details-props-types";
 import { Invoice } from "@/src/types/invoice-types";
 import { fetchInvoice } from "@/src/utils/fetch-invoice";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function InvoiceDetailsPage({ params }: InvoiceDetailsProps) {
   const [invoice, setInvoice] = useState<Invoice | null>(null);
@@ -14,29 +15,29 @@ export default function InvoiceDetailsPage({ params }: InvoiceDetailsProps) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const loadInvoice = async () => {
-      try {
-        setLoading(true);
-        setError(null);
-        const invoice = await fetchInvoice(params);
-        if (!invoice) {
-          setError("Facture non trouvée");
-          return;
-        }
-        setInvoice(invoice);
-        setInitialInvoice(invoice);
-        setLoading(false);
-      } catch (error) {
-        setError("Erreur lors du chargement de la facture.");
-      } finally {
-        setLoading(false);
+  const loadInvoice = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const invoice = await fetchInvoice(params);
+      if (!invoice) {
+        setError("Facture non trouvée");
+        return;
       }
-    };
+      setInvoice(invoice);
+      setInitialInvoice(invoice);
+    } catch (error) {
+      setError("Erreur lors du chargement de la facture.");
+    } finally {
+      setLoading(false);
+    }
+  }, [params]);
 
+  useEffect(() => {
     loadInvoice();
   }, [params]);
 
+  // ----- check if loading state is true and return loading screen -----
   if (loading) {
     return (
       <Container>
@@ -48,6 +49,17 @@ export default function InvoiceDetailsPage({ params }: InvoiceDetailsProps) {
     );
   }
 
+  // ----- check if error state is not null and return error screen -----
+  if (error) {
+    return (
+      <Container>
+        <GoBackButton href="dashboard" />
+        <div className="text-red-500 text-center mt-4">{error}</div>
+      </Container>
+    );
+  }
+
+  // ----- check if invoice state is null and return not found screen -----
   if (!invoice) {
     return (
       <Container>
@@ -59,22 +71,13 @@ export default function InvoiceDetailsPage({ params }: InvoiceDetailsProps) {
     );
   }
 
-  if (error) {
-    return (
-      <Container>
-        <GoBackButton href="dashboard" />
-        <div className="text-red-500 text-center mt-4">{error}</div>
-      </Container>
-    );
-  }
-
   return (
     <Container>
       <GoBackButton href={`dashboard`} />
 
       <div className="mt-6">
         <h1 className="text-2xl font-bold mb-4">Détails de la facture</h1>
-        <p className="text-lg">Facture - {invoice.id}</p>
+        <Badge className="text-md">Facture - {invoice.id}</Badge>
       </div>
     </Container>
   );
